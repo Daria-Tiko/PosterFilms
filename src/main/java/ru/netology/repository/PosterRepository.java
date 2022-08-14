f1aef6e068b84d1329cad996b61a536ac40c5673
Darya Tishchenko QA-41 <tdaniel@bk.ru>
2022-08-14 20:20:36 +0400

    Add PosterRepository

---
+++ src/main/java/ru/netology/repository/PosterRepository.java
@@ -0,0 +1,49 @@
+package ru.netology.repository;
+
+import ru.netology.poster.Film;
+
+public class PosterRepository {
+    private Film[] films = new Film[0];
+
+    public void save(Film movie) {
+        Film[] tmp = new Film[films.length +1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        films = tmp;
+    }
+
+    public Film[] findAll() {
+        return films;
+    }
+
+    public Film findById(int id) {
+        for (Film movie : films) {
+            if (movie.getId() == id) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        if (findById(id) != null) {
+            Film[] tmp = new Film[films.length - 1];
+            int index = 0;
+            for (Film movie : films) {
+                if (movie.getId() != id) {
+                    tmp[index] = movie;
+                    index++;
+                }
+            }
+            films = tmp;
+        }
+    }
+
+    public void removeAll() {
+        films = new Film[0];
+    }
+
+
+}
--- src/test/java/ru/netology/poster/PosterManagerTest.java
+++ src/test/java/ru/netology/poster/PosterManagerTest.java
@@ -1,103 +1,43 @@
 package ru.netology.poster;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.repository.PosterRepository;
+
+import static org.mockito.Mockito.*;
+
+
 
 public class PosterManagerTest {
+    PosterRepository repo = Mockito.mock(PosterRepository.class);
+    PosterManager manager = new PosterManager(repo);
 
-    PosterManager films = new PosterManager();
-
-    Film film1 = new Film("Рататуй", "мульфильм");
-    Film film2 = new Film("Холоп", "комедия");
-    Film film3 = new Film("Мстители", "фантастика");
-    Film film4 = new Film("Веном", "ужасы");
-    Film film5 = new Film("Пираты Карибсского моря", "приключения");
-    Film film6 = new Film("Титаник", "мелодрама");
-    Film film7 = new Film("Плохие парни", "боевик");
-    Film film8 = new Film("Дом странных детей мисс Перегрин", "фэнтези");
-    Film film9 = new Film("Домовой", "семейный");
-    Film film10 = new Film("Хакер", "криминал");
-    Film film11 = new Film("Гордость и предубеждение", "исторический");
-
-    @BeforeEach
-    public void setup() {
-
-        films.addNewFilm(film1);
-        films.addNewFilm(film2);
-        films.addNewFilm(film3);
-        films.addNewFilm(film4);
-        films.addNewFilm(film5);
-        films.addNewFilm(film6);
-        films.addNewFilm(film7);
-        films.addNewFilm(film8);
-        films.addNewFilm(film9);
-        films.addNewFilm(film10);
-        films.addNewFilm(film11);
-    }
+    Film film1 = new Film("Рататуй", "мульфильм", 1);
+    Film film2 = new Film("Холоп", "комедия", 2);
+    Film film3 = new Film("Мстители", "фантастика", 3);
+    Film film4 = new Film("Веном", "ужасы", 4);
+    Film film5 = new Film("Пираты Карибсского моря", "приключения", 5);
+    Film film6 = new Film("Титаник", "мелодрама",6 );
+    Film film7 = new Film("Плохие парни", "боевик", 7);
+    Film film8 = new Film("Дом странных детей мисс Перегрин", "фэнтези", 8);
+    Film film9 = new Film("Домовой", "семейный",9);
+    Film film10 = new Film("Хакер", "криминал",10);
+    Film film11 = new Film("Гордость и предубеждение", "исторический",11);
 
     @Test
     public void shouldFindAll() {
+        Film[] films = { film1, film2, film3, film4, film5, film6, film7, film8, film9, film10, film11};
+        doReturn(films).when(repo).findAll();
+
         Film[] expected = {film1, film2, film3, film4, film5, film6, film7, film8, film9, film10, film11};
-        Film[] actual = films.findAll();
+        Film[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
+
     }
 
-    @Test
-    public void shouldFinLastAll() {
-        Film[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
-        Film[] actual = films.findLast();
 
-        Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
-    public void shouldFinLastEightFilms() {
 
-        PosterManager films = new PosterManager(8);
-
-        films.addNewFilm(film1);
-        films.addNewFilm(film2);
-        films.addNewFilm(film3);
-        films.addNewFilm(film4);
-        films.addNewFilm(film5);
-        films.addNewFilm(film6);
-        films.addNewFilm(film7);
-        films.addNewFilm(film8);
-        films.addNewFilm(film9);
-        films.addNewFilm(film10);
-        films.addNewFilm(film11);
-
-
-        Film[] expected = {film11, film10, film9, film8, film7, film6, film5, film4};
-        Film[] actual = films.findLast();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldFinLastOneFilms() {
-
-        PosterManager films = new PosterManager(1);
-
-        films.addNewFilm(film1);
-        films.addNewFilm(film2);
-        films.addNewFilm(film3);
-        films.addNewFilm(film4);
-        films.addNewFilm(film5);
-        films.addNewFilm(film6);
-        films.addNewFilm(film7);
-        films.addNewFilm(film8);
-        films.addNewFilm(film9);
-        films.addNewFilm(film10);
-        films.addNewFilm(film11);
-
-
-        Film[] expected = {film11};
-        Film[] actual = films.findLast();
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-}
